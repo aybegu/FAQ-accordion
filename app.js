42707089bcb33592e70022bfd9063219e5d839ff
Ayca Gurler <berfingurler@hotmail.com>
2023-12-04 12:06:46 +0300

    icons added and dropdown

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ for(let i=0; i<questions.length; i++){
         if(parseInt(contentDiv[i].style.height)
         != contentDiv[i].scrollHeight){
             contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
+            icons[i].classList.remove('plus');
+            icons[i].classList.add('minus-icon');
+        }
+        else{
+            contentDiv[i].style.height="0px";
+            icons[i].classList.remove('minus-icon');
+            icons[i].classList.add('plus');
+        }
+
+        for(let j=0; j<contentDiv.length; j++){
+            if(j!==i){
+                contentDiv[j].style.height="0px";
+                icons[j].classList.remove('minus-icon');
+                icons[j].classList.add('plus');
+            }
         }
     });
 }
